25df45da0e85307d2f1a1c239b2253b1afb4c1ff
JerryChang <378090670.qq.com>
2017-10-28 14:21:09 +0800

    Create Project

---
+++ many_kids/src/main/java/com/fulloferrors/many_kids/ManyKidsApplication.java
@@ -0,0 +1,12 @@
+package com.fulloferrors.many_kids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyKidsApplication.class, args);
+	}
+}
